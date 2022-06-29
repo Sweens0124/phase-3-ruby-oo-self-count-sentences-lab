c29f88164dd16c0692d14fd6a42d32e6240e0e6a
Lucas Sweeney <lsweeney0124@gmail.com>
2022-06-29 12:24:05 -0500

    commit tree f3a7b3eb40e97931a5da8e526f4efb996122e989

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,28 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+  def sentence? 
+    if self[-1, 1] == "."
+      true
+    else false
+    end
   end
 
   def question?
-
+    if self[-1, 1] == "?"
+      true
+    else false
+    end
   end
 
   def exclamation?
-
+    if self[-1, 1] == "!"
+      true
+    else false
+    end
   end
 
   def count_sentences
-
+    self.split(/\.|\?|!/).filter { |sentence| !sentence.empty? }.size
   end
 end
